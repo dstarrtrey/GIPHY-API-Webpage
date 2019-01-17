f45ad7f0bf7831383a2e6ce215917577fbc8c4de
David Starr <dstarrtrey@gmail.com>
2019-01-17 13:03:31 -0800

    major style changes and adds OMDB API

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    const topics = ["Blade Runner", "Blade Runner 2049", "Battlestar Gallactica", "Star Wars", "Star Trek TNG", "Altered Carbon", "Black Mirror", "Interstellar", "Ex Machina", "Avengers", "The Matrix", "Terminator", "Mad Max", "I, Robot", "Snowpiercer"];
+    const topics = ["Blade Runner", "Blade Runner 2049", "Battlestar Galactica", "Star Wars", "Star Trek: The Next Generation", "Altered Carbon", "Black Mirror", "Interstellar", "Ex Machina", "Avengers", "The Matrix", "Terminator", "Mad Max", "I, Robot", "Snowpiercer"];
     topics.forEach(topic => {
         $("#btn-collection").append($("<button>").text(topic).val(topic).addClass("topic-btn"));
     });
@@ -8,28 +8,59 @@ $(document).ready(function(){
         const gifRating = $("<div>").text("Rating: "+rating.toUpperCase()).addClass("rating");
         const gif = $("<img>").attr("src", url).addClass("gif-img paused");
         $("#gif-collection").append(gifElement.append(gif, gifRating));
+    };
+    const fill = input => {
+        $("#gif-collection").empty();
+        input.data.forEach(element => {
+            newGif(element.rating, element.images.original_still.url);
+        });
+    };
+    const addMovieData = value =>{
+        $("#movie-data").empty();
+        $.ajax({
+            url: `http://www.omdbapi.com/?apikey=trilogy&t=${value}`,
+            method: "GET"
+        }).then(function(response){
+            const poster = $("<img>").attr("src",response.Poster).addClass("poster");
+            const name = $("<h1>").text(response.Title);
+            const yearGenre = $("<h3>").text(`(${response.Year}) Genre: ${response.Genre}`);
+            const plot = $("<p>").text(response.Plot);
+            $("#movie-data").append(poster, name, yearGenre, plot);
+        });
     }
-    $(".topic-btn").on("click", function(){
+    $("#search").submit(function(event){
+        const searchText = $("#search-text").val();
+        event.preventDefault();
+        if(!topics.includes(searchText)){
+            topics.push(searchText);
+            $("#btn-collection").append($("<button>").text(searchText).val(searchText).addClass("topic-btn"));
+        }
+        // $.ajax({
+        //     url: `https://api.giphy.com/v1/gifs/search?q=${$("#search-text").val()}&api_key=NzFSyDo9wgOybNvbdlUoqAwHtAINPDtx&limit=10`,
+        //     method: "GET"
+        // }).then(function(response){
+        //     fill(response);
+        // });
+        // return false;
+    });
+    $(document).on("click", ".topic-btn", function(){
+        addMovieData(this.value);
         $.ajax({
             url:`https://api.giphy.com/v1/gifs/search?q=${this.value}&api_key=NzFSyDo9wgOybNvbdlUoqAwHtAINPDtx&limit=10`,
             method: "GET"
         }).then(function(response){
-            console.log(response.data);
-            $("#gif-collection").empty();
-            response.data.forEach(element => {
-                newGif(element.rating, element.images.original_still.url);
-            });
-            $(".gif-img").on("click", function(){
-                if($(this).hasClass("paused")){
-                    $(this).addClass("playing").removeClass("paused");
-                    let src = this.src;
-                    src = src.substring(0, src.length-6)+'.gif';
-                    this.src = src;
-                }else{
-                    $(this).addClass("paused").removeClass("playing");
-                    this.src = this.src.substring(0, this.src.length-4)+'_s.gif';
-                }
-            }); 
+            fill(response);
         });
     });
+    $(document).on("click", ".gif-img", function(){
+        if($(this).hasClass("paused")){
+            $(this).addClass("playing").removeClass("paused");
+            let src = this.src;
+            src = src.substring(0, src.length-6)+'.gif';
+            this.src = src;
+        }else{
+            $(this).addClass("paused").removeClass("playing");
+            this.src = this.src.substring(0, this.src.length-4)+'_s.gif';
+        }
+    }); 
 });
